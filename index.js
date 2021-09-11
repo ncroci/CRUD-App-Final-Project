b2c9d6b12bf3603a301a2570a8308ca285571b54
Nicholas Croci <86008143+ncroci@users.noreply.github.com>
2021-09-11 14:52:06 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -21,18 +21,18 @@ class ListService {
     static url = 'https://crudcrud.com/api/b90ca1ec7c0544f288a959f9f5d2ce87/lists';
 
     // Read in CRUD 
-    static getAllLists() {                      // WORKING
+    static getAllLists() { // WORKING
         return $.ajax({
             url: this.url,
             dataType: 'json',
             data: JSON.stringify(),
             contentType: 'application/json',
             type: 'GET'
-        }); 
+        });
     }
-    
+
     // Read in CRUD
-    static getList(list) {                      // WORKING
+    static getList(list) { // WORKING
         return $.ajax({
             url: this.url,
             dataType: 'json',
@@ -43,34 +43,31 @@ class ListService {
     }
 
     //Create in CRUD
-    static createList(list) {                   // WORKING
+    static createList(list) { // WORKING
         return $.ajax({
-            url: this.url, 
+            url: this.url,
             dataType: 'json',
             data: JSON.stringify(list),
             contentType: 'application/json',
             type: 'POST'
-        }); 
+        });
     }
 
     //Update in CRUD
-    static updateList(list) {
-        console.log("Inside of ListService-updateList")
+    static updateList(list, id) {
+        console.log(list);
         // console.log(list);
         return $.ajax({
-            url: this.url,
+            url: this.url + "/" + id,
             dataType: 'json',
-            data: JSON.stringify({
-                name: name,
-                tasks: tasks,
-            }),
+            data: JSON.stringify({ list }),
             contentType: 'application/json',
             type: 'PUT'
-        }); 
+        });
     }
 
     //Delete in CRUD
-    static deleteList(id) {                     // WORKING
+    static deleteList(id) { // WORKING
         return $.ajax({
             url: this.url + "/" + id,
             dataType: 'json',
@@ -108,7 +105,7 @@ class DOMManager {
         for (let list of this.lists) {
             if (list._id == id) {
                 list.tasks.push(new Task($(`#${list._id}-task-name`).val(), $(`#${list._id}-task-date`).val()));
-                ListService.updateList(list)
+                ListService.updateList(list, id)
                     .then(() => {
                         return ListService.getAllLists();
                     })
@@ -123,7 +120,7 @@ class DOMManager {
                 for (let task of list.tasks) {
                     if (task._id == taskId) {
                         list.tasks.splice(list.tasks.indexOf(task), 1);
-                        ListService.updateList(list) 
+                        ListService.updateList(list)
                             .then(() => {
                                 return ListService.getAllLists();
                             })
@@ -148,12 +145,12 @@ class DOMManager {
                     <div class="card-body">
                         <div class="card">
                             <div class"row">
-                                <div class="col-sm">
-                                    <input type="text" id="${list._id}-task-name" class="form-control" placeholder="Task Name">
+                                <div class="col">
+                                    <input type="text" id="${list._id}-task-name" class="form-control" placeholder="New Task">
                                 </div>
-                                <div class="col-sm">
+                                <div class="col">
                                     <input type="date" id="${list._id}-task-date" class="form-control" placeholder="Task Date">
-                                </div>
+                                </div><br>
                             </div>
                             <button id="${list._id}-new-task" onclick="DOMManager.addTask('${list._id}')" class="btn btn-primary form-control"> Add</button>
                         </div>
